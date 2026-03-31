797fb5059b55afbbb41b300ecfc9fa8a5c8dd3c5
diegojosegv16-coder <diegojosegv16@gmail.com>
2026-03-31 17:04:46 -0300

    Update ejercicio4.py

--- Semana5/ejercicio4.py
+++ Semana5/ejercicio4.py
@@ -4,8 +4,19 @@ Dado un número entero positivo N, contar cuántos números pares existen entre 
 """
 
 def contar_pares_ciclo(n):
-    pass
-
+    resultado = 0
+    for i in range (1,n+1):
+        if i % 2 == 0:
+            resultado = resultado + 1
+    return resultado
+print(contar_pares_ciclo(6))
 
 def contar_pares_recursivo(n):
-    pass
+    if n == 0:
+        return 0
+    elif n%2 == 0:
+        return 1 + contar_pares_recursivo(n-1)
+    else:
+        0 + contar_pares_recursivo(n-1)
+print(contar_pares_recursivo(6))
+    
